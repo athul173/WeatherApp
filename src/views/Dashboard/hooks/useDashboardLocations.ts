c9f5de1854da50c519cd95b1622090281e17085c
Athul Mithran <athulakhil173@gmail.com>
2024-11-06 13:54:07 +0100

    fix: location not granted

--- src/views/Dashboard/hooks/useDashboardLocations.ts
+++ src/views/Dashboard/hooks/useDashboardLocations.ts
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useCallback, useEffect, useRef, useState} from 'react';
 import Geolocation from '@react-native-community/geolocation';
 import {PermissionsAndroid, Platform} from 'react-native';
 
@@ -21,54 +21,47 @@ const initialLocations = [
 
 const useDashboardLocations = () => {
   const [locations, setLocations] = useState(initialLocations);
-  const [locationPermissionGranted, setLocationPermissionGranted] =
-    useState(false);
+
+  const locationCleanupRef = useRef(false);
+
+  const addCurrentLocation = useCallback(() => {
+    Geolocation.getCurrentPosition(
+      position => {
+        setLocations([
+          {
+            name: 'My location',
+            coordinates: {
+              latitude: position.coords.latitude.toString(),
+              longitude: position.coords.longitude.toString(),
+            },
+          },
+          ...locations,
+        ]);
+      },
+      error => {
+        console.log(error);
+      },
+    );
+  }, [locations]);
 
   useEffect(() => {
-    const requestLocationPermission = async () => {
-      if (Platform.OS === 'android') {
-        try {
-          const granted = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-          );
-          setLocationPermissionGranted(
-            granted === PermissionsAndroid.RESULTS.GRANTED,
-          );
-        } catch (err) {
-          console.warn(err);
-        }
-      }
-      if (Platform.OS === 'ios') {
-        // TODO: add permissions handler for iOS
-      }
-    };
-
-    requestLocationPermission()
-      .then()
-      .catch(() => {});
-  }, []);
-
-  const addCurrentLocation = () => {
-    if (locationPermissionGranted) {
-      Geolocation.getCurrentPosition(
-        position => {
-          setLocations([
-            {
-              name: 'My location',
-              coordinates: {
-                latitude: position.coords.latitude.toString(),
-                longitude: position.coords.longitude.toString(),
-              },
-            },
-            ...locations,
-          ]);
-        },
-        error => {
-          console.log(error);
+    if (locationCleanupRef.current) {
+      return;
+    }
+    if (Platform.OS === 'android') {
+      PermissionsAndroid.check('android.permission.ACCESS_FINE_LOCATION').then(
+        result => {
+          console.log('Location granted is ', result);
+          if (result) {
+            addCurrentLocation();
+          }
         },
       );
     }
-  };
+    return () => {
+      locationCleanupRef.current = true;
+    };
+  }, [addCurrentLocation]);
 
   return {locations, addLocationHandler: addCurrentLocation};
 };
