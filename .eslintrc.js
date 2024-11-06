9bb224d9f835aaaad5a2eec1a9d225fde60cce2f
Athul Mithran <athulakhil173@gmail.com>
2024-11-06 17:19:09 +0100

    chore: add configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,54 @@
+const baseStyleRules = require('eslint-config-airbnb-base/rules/style');
+
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'airbnb-typescript', 'prettier'],
+  plugins: [
+    // For some reason we have to manually specify the import plugin here even
+    //  though it should be loaded by airbnb-typescript.
+    'import',
+  ],
+  env: {
+    jest: true,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json', '.native.js'],
+      },
+    },
+  },
+  parserOptions: {
+    project: ['./tsconfig.json'],
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/static-property-placement': ['error', 'static public field'],
+    // Allow for .. of syntax - regenerator runtime is not needed in RN js anyway
+    'no-restricted-syntax': baseStyleRules.rules['no-restricted-syntax'].filter(
+      rule => typeof rule === 'string' || rule.selector !== 'ForOfStatement',
+    ),
+  },
+  overrides: [
+    {
+      files: ['*.spec.tsx'],
+      rules: {
+        'react/jsx-props-no-spreading': 'off',
+        'react/require-default-props': 'off',
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: 'variable',
+            format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+            filter: {
+              regex: '^(UNSAFE_).+$',
+              match: false,
+            },
+          },
+        ],
+      },
+    },
+  ],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['./jest-setup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native(-.*)?|@react-native(-community)?|react-redux)/)',
+  ],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
 };
